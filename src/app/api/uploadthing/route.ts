8e043292e868c5fa838735f803ee41f9ec38ead8
meniem13 <meniemnasr84@gmail.com>
2024-09-26 10:48:07 +0300

    ...

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,7 +1,7 @@
-import { createRouteHandler } from 'uploadthing/next'
-
-import { ourFileRouter } from './core'
+import { createRouteHandler } from "uploadthing/next";
+import { ourFileRouter } from "./core";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-})
+});
